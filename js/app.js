fabd3e0fea7cf19dd4df210bb17510071fed1021
won <wjin0352@gmail.com>
2016-04-19 17:41:11 -0400

    feature/#1-inpiration-getter-api-endpoint added jquery to grab results from 2nd input box for tags

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // this function takes the question object returned by the StackOverflow request
 // and returns new result to be appended to DOM
 var showQuestion = function(question) {
-	
+
 	// clone our result template code
 	var result = $('.templates .question').clone();
-	
+
 	// Set the question properties in result
 	var questionElem = result.find('.question-text a');
 	questionElem.attr('href', question.link);
@@ -49,15 +49,15 @@ var showError = function(error){
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-	
+
 	// the parameters we need to pass in our request to StackOverflow's API
-	var request = { 
+	var request = {
 		tagged: tags,
 		site: 'stackoverflow',
 		order: 'desc',
 		sort: 'creation'
 	};
-	
+
 	$.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
@@ -91,4 +91,13 @@ $(document).ready( function() {
 		var tags = $(this).find("input[name='tags']").val();
 		getUnanswered(tags);
 	});
+
+	$('.inspiration-getter').submit(function (e) {
+		e.preventDefault();
+		// clears out previous search results
+		$('.results').html('');
+		// get value of tags user submitted
+		var tags_inspiration = $(this).find("input[name='answerers']").val();
+		getUnanswered(tags_inspiration);
+	})
 });
