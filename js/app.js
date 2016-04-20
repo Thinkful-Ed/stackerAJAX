6145cf2d4c946ba8dd799e893a7da65e30258e78
won <wjin0352@gmail.com>
2016-04-20 16:23:30 -0400

    created api endpoint for top answerers, setup hidden html and jquery handlers

--- js/app.js
+++ js/app.js
@@ -32,6 +32,34 @@ var showQuestion = function(question) {
 };
 
 
+
+
+var showTopAnswerersQuestion = function(question) {
+
+	// clone our result template code
+	var result = $('.top-answerers .question-top').clone();
+
+	// Set the question properties in result
+	var imageElem = result.find('.image');
+	imageElem.attr('src', question.user.profile_image);
+	imageElem.text(question.user.display_name);
+
+	var imageAnchor = result.find('#answerers-image');
+	imageAnchor.attr('href', question.user.link);
+
+	// set some properties related to asker
+	var asker = result.find('.asker');
+	asker.html('<p>Name: <a target="_blank" '+
+		'href=http://stackoverflow.com/users/' + question.user.user_id + ' >' +
+		question.user.display_name +
+		'</a></p>' +
+		'<p>Reputation: ' + question.user.reputation + '</p>'
+	);
+
+	return result;
+};
+
+
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
 var showSearchResults = function(query, resultNum) {
@@ -66,8 +94,11 @@ var getUnanswered = function(tags) {
 	})
 	.done(function(result){ //this waits for the ajax to return with a succesful promise object
 		var searchResults = showSearchResults(request.tagged, result.items.length);
+		// console.log(request.tagged);
+		// console.log(result.items.length);
 
 		$('.search-results').html(searchResults);
+
 		//$.each is a higher order function. It takes an array and a function as an argument.
 		//The function is executed once for each item in the array.
 		$.each(result.items, function(i, item) {
@@ -82,12 +113,42 @@ var getUnanswered = function(tags) {
 };
 
 // takes user results for a tag and finds top answerers for it
-var getTopAnswerers = function(tags) {
+var getTopAnswerers = function(tag) {
+	// parameters needed to pass our request to stackoverflow's api
+	var request = {
+		tag: tag,
+		site: 'stackoverflow',
+		period: 'all_time'
+	};
 
+	$.ajax({
+		url: "http://api.stackexchange.com/2.2/tags/"+ tag + "/top-answerers/all_time?",
+		// 2.2/tags/{tag}/top-answerers/all_time?site=stackoverflow
+		data: request,
+		dataType: "jsonp",
+		type: "GET",
+	})
+	.done(function(result){
+		var searchResults = showSearchResults(request.tag, result.items.length);
+		console.log(request);
+		console.log(result);
+		$('.search-results').html(searchResults);
+
+		$.each(result.items, function(i, item){
+			var question = showTopAnswerersQuestion(item);
+			$('.results').append(question);
+			console.log(item);
+		});
+	})
+	.fail(function(jqXHR, error) {
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
 };
 
 
 $(document).ready( function() {
+	// event handler for unanswered question
 	$('.unanswered-getter').submit( function(e){
 		e.preventDefault();
 		// zero out results if previous search has run
@@ -97,12 +158,13 @@ $(document).ready( function() {
 		getUnanswered(tags);
 	});
 
+	// event handler for top answerers
 	$('.inspiration-getter').submit(function (e) {
 		e.preventDefault();
 		// clears out previous search results
 		$('.results').html('');
 		// get value of tags user submitted
-		var tags_inspiration = $(this).find("input[name='answerers']").val();
-		getUnanswered(tags_inspiration);
+		var tag_inspiration = $(this).find("input[name='answerers']").val();
+		getTopAnswerers(tag_inspiration);
 	})
 });
