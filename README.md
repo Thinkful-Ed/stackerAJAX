ef9f945d4eb9762f883fabcfa4433eb3cda7ae17
JP DeVries <mail@devries.jp>
2016-08-10 00:48:32 +0200

    Sync Endpoints (Node Server)
    full support (.no-js)

---
+++ README.md
@@ -0,0 +1,40 @@
+stackerForm
+========
+
+Progressively enhanced fork of [Thinkful-Ed/stackerAJAX](https://github.com/Thinkful-Ed/stackerAJAX).
+
+## Preview
+Have a look at the synchronous and asynchronous endpoints in&nbsp;action.  
+
+![](http://j4p.us/0E243Q2G0r3z/stackerform.gif)
+
+## Getting Started
+
+In order to build our front end assets, you need to have Node.js/npm latest and git 1.7 or later.
+(Earlier versions might work OK, but are not tested.)
+
+For Windows you have to download and install [git](http://git-scm.com/downloads) and [Node.js](http://nodejs.org/download/).
+
+Mac OS users should install [Homebrew](http://mxcl.github.com/homebrew/). Once Homebrew is installed, run `brew install git` to install git,
+and `brew install node` to install Node.js.
+
+Linux/BSD users should use their appropriate package managers to install git and Node.js, or build from source
+if you swing that way. Easy-peasy.
+
+## Getting Started
+First, clone a copy of this git repo by running:
+
+```bash
+git clone -b grunt git@github.com:jpdevries/stackerForm.git
+```
+
+Then cd into the `stackerForm` folder and install the Node&nbsp;dependencies:
+```bash
+cd stackerForm
+npm install
+```
+
+You should now be able to run the Node&nbsp;server!
+```bash
+npm run start
+```
--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 // this function takes the question object returned by the StackOverflow request
 // and returns new result to be appended to DOM
 var showQuestion = function(question) {
-	
+
 	// clone our result template code
 	var result = $('.templates .question').clone();
-	
+
 	// Set the question properties in result
 	var questionElem = result.find('.question-text a');
 	questionElem.attr('href', question.link);
@@ -31,6 +31,16 @@ var showQuestion = function(question) {
 	return result;
 };
 
+var showAnswer = function(answer) {
+	var result = $('.templates .answerer').clone();
+
+	result.find('.answerer-name a').text(answer.user.display_name);
+	result.find('.answerer-count').text(answer.post_count);
+	result.find('.answerer-count').text(answer.score);
+
+	return result;
+};
+
 
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
@@ -49,15 +59,15 @@ var showError = function(error){
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-	
+
 	// the parameters we need to pass in our request to StackOverflow's API
-	var request = { 
+	var request = {
 		tagged: tags,
 		site: 'stackoverflow',
 		order: 'desc',
 		sort: 'creation'
 	};
-	
+
 	$.ajax({
 		url: "http://api.stackexchange.com/2.2/questions/unanswered",
 		data: request,
@@ -81,6 +91,40 @@ var getUnanswered = function(tags) {
 	});
 };
 
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getTopAnswerers = function(tags) {
+
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = {
+		tagged: tags,
+		site: 'stackoverflow',
+		order: 'desc',
+		sort: 'creation'
+	};
+
+	$.ajax({
+		url: 'http://api.stackexchange.com/2.2/tags/' + tags + '/top-answerers/all_time',
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+	.done(function(result){ //this waits for the ajax to return with a succesful promise object
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			var answer = showAnswer(item);
+			$('.results').append(answer);
+		});
+	})
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
 
 $(document).ready( function() {
 	$('.unanswered-getter').submit( function(e){
@@ -88,7 +132,15 @@ $(document).ready( function() {
 		// zero out results if previous search has run
 		$('.results').html('');
 		// get the value of the tags the user submitted
-		var tags = $(this).find("input[name='tags']").val();
+		var tags = $(this).find("input[name='tagged']").val();
 		getUnanswered(tags);
+	});
+	$('.inspiration-getter').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='tagged']").val();
+		getTopAnswerers(tags);
 	});
 });
